653ff4837fcef9b5fb13ccecb9c536d201e9a896
refat <refat@192.168.86.174>
2020-01-11 21:06:11 -0500

    whatever

--- Smarttech-FirstProject-Java/src/com/usa/GithubPractice/GithubPractice.java
+++ Smarttech-FirstProject-Java/src/com/usa/GithubPractice/GithubPractice.java
@@ -3,15 +3,23 @@ package com.usa.GithubPractice;
 public class GithubPractice {
 
 	public static void main(String[] args) {
-		
-		
+
 		int a = 2;
 		int b = 3;
-		int c= a+b;
-		
-		System.out.println(c);
-		
-		
-	}
+		int c = a + b;
 
+		System.out.println(c);
+
+	
+
+	if (a+b==5) {
+		System.out.println("a plus b is equal to 5");
+	}
+	
+	else {
+		System.out.println("it's not correct");
+	}
+	
+	}
+	
 }
