052b86b662a59898cb310b9295206499b4fa053f
refat <refat@192.168.86.174>
2020-01-11 20:25:19 -0500

    Commmit by Refat after creating package and class

---
+++ Smarttech-FirstProject-Java/src/com/usa/GithubPractice/GithubPractice.java
@@ -0,0 +1,17 @@
+package com.usa.GithubPractice;
+
+public class GithubPractice {
+
+	public static void main(String[] args) {
+		
+		
+		int a = 2;
+		int b = 3;
+		int c= a+b;
+		
+		System.out.println(c);
+		
+		
+	}
+
+}
